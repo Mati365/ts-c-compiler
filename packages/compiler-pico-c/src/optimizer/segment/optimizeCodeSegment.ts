e02f866c83f5d183821bfce5e6696d79a8597846
Mateusz Baginski <cziken58@gmail.com>
2022-06-24 08:49:37 +0200

    fix: fix typo

--- packages/compiler-pico-c/src/optimizer/segment/optimizeCodeSegment.ts
+++ packages/compiler-pico-c/src/optimizer/segment/optimizeCodeSegment.ts
@@ -3,7 +3,7 @@ import * as R from 'ramda';
 import {IRCodeSegmentBuilderResult} from '@compiler/pico-c/frontend/ir/generator';
 import {optimizeInstructionsBlock} from '../block';
 
-export function optimizeCodeSegment(segment: IRCodeSegmentBuilderResult): IRCodeSegmentBuilderResult{
+export function optimizeCodeSegment(segment: IRCodeSegmentBuilderResult): IRCodeSegmentBuilderResult {
   return {
     ...segment,
     blocks: R.mapObjIndexed(
