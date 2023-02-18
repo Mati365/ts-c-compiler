432b4b3bd09fccb8c28e8480b8776cc8d9da25a0
Mateusz Baginski <cziken58@gmail.com>
2023-02-18 10:54:25 +0100

    feat: add test example

--- src/cli.ts
+++ src/cli.ts
@@ -2,39 +2,9 @@ import 'source-map-support/register';
 import { ccompiler, CCompilerOutput } from '@compiler/pico-c';
 
 ccompiler(/* cpp */ `
-  // todo: letters[0] should be truncated
-  // huge issue, truncating type in IR!
-  // void main() {
-  //   char letters[] = "Hello world";
-
-  //   char b = letters[0];
-  //   int a = letters[0] * 2;
-  // }
-
-  // todo: Optimize
-  // add bx, 32                ; %t{3}: int*2B = %t{0}: int*2B plus %32: int2B
-  // mov ax, [bx]              ; %t{4}: int2B = load %t{3}: int*2B
-  // struct Point {
-  //   int x, y;
-  //   int dupa[10];
-  //   char c;
-  // };
-
-  // void main() {
-  //   struct Point point[] = { { .y = 6 }, { .x = 2 } };
-  //   point[1].dupa[2]++;
-  // }
-
+  void sum(int a, int b) {}
   void main() {
-    char a = 'a';
-
-    if (a > 4) {
-      int k = 0;
-    } else if (a < 5) {
-      int j = 4;
-    } else {
-      int c = 4;
-    }
+    sum(4, 4);
   }
 `).match({
   ok: result => {
@@ -48,10 +18,3 @@ ccompiler(/* cpp */ `
     console.error(error);
   },
 });
-
-/**
- * Optimize to AL:
- *
- * and ax, 0xff
- * mov word [bp - 12], ax    ; *(b{0}: char*2B) = store %t{1}: int2B
- */
