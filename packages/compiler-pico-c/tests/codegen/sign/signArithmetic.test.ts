5a86e0321964fa167fbc914be6e811326e967f6a
Mateusz Baginski <cziken58@gmail.com>
2023-06-11 14:30:56 +0200

    feat: add unsigned mul

--- packages/compiler-pico-c/tests/codegen/sign/signArithmetic.test.ts
+++ packages/compiler-pico-c/tests/codegen/sign/signArithmetic.test.ts
@@ -48,4 +48,50 @@ describe('Sign arithmetic', () => {
       `);
     });
   });
+
+  describe('Multiplication', () => {
+    test('signed mul', () => {
+      expect(/* cpp */ `
+        void main() {
+          int d, e;
+          int f = d * e;
+        }
+      `).toCompiledAsmBeEqual(`
+        cpu 386
+        ; def main():
+        @@_fn_main:
+        push bp
+        mov bp, sp
+        sub sp, 6
+        mov ax, [bp - 2]
+        imul ax, word [bp - 4]    ; %t{2}: int2B = %t{0}: int2B mul %t{1}: int2B
+        mov word [bp - 6], ax     ; *(f{0}: int*2B) = store %t{2}: int2B
+        mov sp, bp
+        pop bp
+        ret
+      `);
+    });
+
+    test('unsigned mul', () => {
+      expect(/* cpp */ `
+        void main() {
+          unsigned int d, e;
+          unsigned int f = d * e;
+        }
+      `).toCompiledAsmBeEqual(`
+        cpu 386
+        ; def main():
+        @@_fn_main:
+        push bp
+        mov bp, sp
+        sub sp, 6
+        mov ax, [bp - 2]
+        mul word [bp - 4]         ; %t{2}: unsigned int2B = %t{0}: unsigned int2B mul %t{1}: unsigned int2B
+        mov word [bp - 6], ax     ; *(f{0}: unsigned int*2B) = store %t{2}: unsigned int2B
+        mov sp, bp
+        pop bp
+        ret
+      `);
+    });
+  });
 });
