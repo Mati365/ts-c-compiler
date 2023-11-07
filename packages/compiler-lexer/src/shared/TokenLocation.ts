45a7f499a0e22eca3367961fbdfb65ee50946399
Mateusz Baginski <cziken58@gmail.com>
2023-11-07 11:43:28 +0100

    feat: add pathname to `TokenLocation`

--- packages/compiler-lexer/src/shared/TokenLocation.ts
+++ packages/compiler-lexer/src/shared/TokenLocation.ts
@@ -2,16 +2,22 @@
  * Provides row/column position of token inside source code
  */
 export class TokenLocation {
-  constructor(public row: number = 0, public column: number = 0) {}
+  constructor(
+    public row: number = 0,
+    public column: number = 0,
+    public pathname: string | null = null,
+  ) {}
 
   append(row: number, column: number = 0): TokenLocation {
-    return new TokenLocation(this.row + row, this.column + column);
+    return new TokenLocation(
+      this.row + row,
+      this.column + column,
+      this.pathname,
+    );
   }
 
   clone(): TokenLocation {
-    const { row, column } = this;
-
-    return new TokenLocation(row, column);
+    return new TokenLocation(this.row, this.column, this.pathname);
   }
 
   toString() {
