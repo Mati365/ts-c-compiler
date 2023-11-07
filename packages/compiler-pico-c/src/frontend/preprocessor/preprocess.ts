7c2d4a2a915bbf28058f3ca9cecb28dbc1d86440
Mateusz Baginski <cziken58@gmail.com>
2023-11-07 11:11:59 +0100

    refactor: rename `preprocessTokens` to `interpret`

--- packages/compiler-pico-c/src/frontend/preprocessor/preprocess.ts
+++ packages/compiler-pico-c/src/frontend/preprocessor/preprocess.ts
@@ -4,13 +4,13 @@ import { pipe } from 'fp-ts/function';
 import type { Token } from '@ts-c-compiler/lexer';
 
 import { CPreprocessorError, CPreprocessorErrorCode } from './grammar';
-import { preprocessTokens } from './interpreter';
+import { interpret } from './interpreter';
 
 export const safePreprocess = (
   tokens: Token[],
 ): E.Either<CPreprocessorError[], Token[]> => {
   try {
-    return pipe(tokens, preprocessTokens, E.right);
+    return pipe(tokens, interpret, E.right);
   } catch (e) {
     e.code = e.code ?? CPreprocessorErrorCode.SYNTAX_ERROR;
 
