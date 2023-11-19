053404c4dc391778175d4d9c9a6b1b7e886b62b1
Mateusz Baginski <cziken58@gmail.com>
2023-11-19 14:44:53 +0100

    feat: add `va_list` typedef

--- packages/compiler-pico-c/src/fs/stdarg.h.ts
+++ packages/compiler-pico-c/src/fs/stdarg.h.ts
@@ -1,5 +1,9 @@
 export const STD_ARG_CONTENT_HEADER = /* c */ `
-  #define va_start(v, l)	__builtin_va_start(v, l)
-  #define va_end(v)	__builtin_va_end(v)
-  #define va_arg(v,l)	__builtin_va_arg(v, l)
+  #define va_start(v, l)  __builtin_va_start(v, l)
+  #define va_end(v)       __builtin_va_end(v)
+  #define va_arg(v,l)     __builtin_va_arg(v, l)
+
+  typedef struct {
+    void *overflow_arg_area;
+  } va_list[1];
 `;
