efe7f7a43e542f6f29147264d2cc3fb92fc79aa3
Mateusz Baginski <cziken58@gmail.com>
2024-02-10 10:20:48 +0100

    fix: eslint memleak

--- apps/x86-16-vm/src/client/screen/Screen.tsx
+++ apps/x86-16-vm/src/client/screen/Screen.tsx
@@ -1,7 +1,7 @@
 import { useRef, useEffect } from 'react';
 import { Buffer } from 'buffer';
 import { X86CPU, VGARenderLoopDriver } from '@ts-c-compiler/x86-cpu';
-import { ScreenCanvasWrapper, ScreenWrapper } from './screen.styled';
+import { ScreenCanvasWrapper, ScreenWrapper } from './Screen.styled';
 
 type ScreenProps = {
   binary: number[];
