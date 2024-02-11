63d72feddf466c306abc87021146350809230755
Mateusz Baginski <cziken58@gmail.com>
2024-02-11 16:00:14 +0100

    fix: improve formatting

--- apps/site/src/modules/Editor/EditorStateProvider/EditorStateProvider.ts
+++ apps/site/src/modules/Editor/EditorStateProvider/EditorStateProvider.ts
@@ -44,14 +44,15 @@ int main() {
   kernel_screen_clear();
 
   for (i = 0; i < rows; i++) {
-    for (space = 1; space <= rows - i; space++)
-       printf("  ");
+    for (space = 1; space <= rows - i; space++) {
+      printf("  ");
+    }
 
     for (j = 0; j <= i; j++) {
       if (j == 0 || i == 0) {
         coef = 1;
       } else {
-         coef = coef * (i - j + 1) / j;
+        coef = coef * (i - j + 1) / j;
       }
 
       printf("%4d", coef);
